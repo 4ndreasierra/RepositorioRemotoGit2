129f356d10b7b1b73d512b378d82e07db4aec525
alumno <alumno@linux-vbox-1dam>
2023-02-28 17:07:14 +0100

    creacion de clase 2

---
+++ src/Paquete2/Clase2.java
@@ -0,0 +1,11 @@
+package Paquete2;
+
+public class Clase2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Se ha creado una nueva rama denominada rama 1 dentro de la vista git");
+		System.out.println("A continuación se van a actualizar los cambios subiendolos al repositorio remoto");
+	}
+
+}
