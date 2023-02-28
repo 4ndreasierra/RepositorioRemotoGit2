6f0e45d2949836031503705a54deb72cbfe05bcc
alumno <alumno@linux-vbox-1dam>
2023-02-28 17:07:22 +0100

    creacion de clase 3

---
+++ src/Paquete2/Clase3.java
@@ -0,0 +1,10 @@
+package Paquete2;
+
+public class Clase3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Se ha creado una segunda rama denominada rama 2");
+	}
+
+}
