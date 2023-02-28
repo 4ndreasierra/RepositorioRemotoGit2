25c6ee35d6924d759ee86d7317fe7793efe7020c
alumno <alumno@linux-vbox-1dam>
2023-02-28 16:47:41 +0100

    creacion del proyecto (1)

---
+++ src/Paquete2/Principal.java
@@ -0,0 +1,10 @@
+package Paquete2;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Control de versiones en github");
+	}
+
+}
